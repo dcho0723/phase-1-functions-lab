ac9446b385cda2117681bd13bafd61e0d1daf270
dcho0723 <dcho0723@yahoo.com>
2021-06-28 13:16:42 -0400

    commit tree 73752fac6e8689614ca5999be2f558a7a22e9c43

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (streetNum) => {
+    if (streetNum >= 42) {
+        return streetNum - 42;
+    } else {
+        return 42 - streetNum;
+    }
+}
+
+const distanceFromHqInFeet = (streetNum) => {
+    if (streetNum >= 42) {
+        return ((streetNum - 42) * 264);
+    } else {
+        return ((42 - streetNum) * 264);
+    }
+}
+
+const distanceTravelledInFeet = (street1, street2) => {
+    if (street1 > street2) {
+        return ((street1 - street2) * 264);
+    } else {
+        return ((street2 - street1) * 264);
+    }
+}
+
+const calculatesFarePrice = (street1, street2) => {
+    if (distanceTravelledInFeet(street1, street2) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(street1, street2) > 400 && distanceTravelledInFeet(street1, street2) <= 2000) {
+        return 2.56;
+    } else if(distanceTravelledInFeet(street1, street2) > 2000 && distanceTravelledInFeet(street1, street2) <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
